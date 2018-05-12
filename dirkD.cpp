3362d1fc9b4d6b3e7f573e3275e7bcf51450f8b9
Dirk <dirkduclos@gmail.com>
2018-05-12 00:35:41 -0500

    k&r,additional funct. for testing

--- dirkD.cpp
+++ dirkD.cpp
@@ -36,7 +36,6 @@ using namespace std;
 typedef double Flt;
 typedef double Vec[3];
 typedef Flt	Matrix[4][4];
-
 #define PI 3.141592653589793
 #define ALPHA 1
 const float GRAVITY =  -0.2f;
@@ -45,7 +44,35 @@ const float GRAVITY =  -0.2f;
 extern Global g;
 extern Timers timers;
 
+//Extern functions
+extern void spawnEnemy(Global&,const float, const float);
+
 int killCounter = 0;
+
+//enemy health counters for each wave
+int health1 = 0;
+int health2 = 0;
+int health3 = 0;
+int health4 = 0;
+int health5 = 0;
+int health6 = 0;
+int health7 = 0;
+int health8 = 0;
+int health9 = 0;
+int health10 = 0;
+
+//Hit detection for each wave of enemies
+bool hit1 = false;
+bool hit2 = false;
+bool hit3 = false;
+bool hit4 = false;
+bool hit5 = false;
+bool hit6 = false;
+bool hit7 = false;
+bool hit8 = false;
+bool hit9 = false;
+bool hit10 = false;
+
 
 //Check for character colission with window edges
 void detectCharWallColission(int posx, int posy, Global &g)
@@ -80,6 +107,10 @@ void detectCharWallColission(int posx, int posy, Global &g)
     }
 }
 
+/*
+This is object oriented approach to Colission
+deletes object from mem, as well as bullet
+*/
 void calculateCollisionOfBullet(Bullet* b, Global& g)
 {
     std::vector<Enemy*> remove;
@@ -118,6 +149,11 @@ void calculateCollisionOfBullet(Bullet* b, Global& g)
 }
 
 
+/*
+function to draw box around enemies
+this box function also updates pos as
+each enemy moves across the screen
+*/
 void drawBox(int x,int y, int posx, int posy, Global&g)
 {
     glPushMatrix();
@@ -138,18 +174,170 @@ void drawBox(int x,int y, int posx, int posy, Global&g)
 }
 
 /*
-void detectBullEnemyColission(Bullet *b, Enemy *e, Global *g)
-{
-    //cout << "bullet pos x: " << (int)b->pos[0]+5 << endl;
-    //cout << "shape x: " << s.centerx << endl;
-           if (b->pos[0] > e->pos[0]) {
-           Enemy *savee = e->next;
-           deleteEnemy(g,e);
-           e = savee;
-           g->nenemies--;
-           }
-           if (e == NULL)
-       			exit(0);
-       		e = e->next;
-}
+Test if bullet hit enemy
+This checks colission for graphical approach
+This is old code before object approach
 */
+bool detectBullEnemyColission(Global &g)
+{
+    for (int i = 0; i < g.nbullets; i++) {
+        if (g.barr[i].pos[0] > (float)g.centerx-10 && g.barr[i].pos[0] < (float)g.centerx+10 &&
+            g.barr[i].pos[1] > (float)g.centery-30 && g.barr[i].pos[1] < (float)g.centery+30) {
+                return true;
+        }
+    }
+    return false;
+}
+
+/*
+function to spawn enemies for graphical approach
+this is old function, drawEnemy is now used
+*/
+void spawnEnemy(Global&g, const float posx, const float posy)
+{
+    int cx = g.exres/posx;
+    int cy = g.eyres/posy;
+
+    g.centerx = cx;
+    g.centery = cy;
+
+    extern void drawBox(int,int,int,int, Global&);
+    drawBox(posx,posy,cx,cy,g);
+
+    float h = 30.0;
+    float w = h * 0.5; //0.5 h = 50.0
+
+    glPushMatrix();
+    glColor3f(1.0, 1.0, 1.0);
+    glBindTexture(GL_TEXTURE_2D, g.alienTexture);
+
+    glEnable(GL_ALPHA_TEST);
+    glAlphaFunc(GL_GREATER, 0.0f);
+    glColor4ub(255,255,255,255);
+    int ix = g.walkFrame % 4;
+    int iy = 0;
+    if (g.walkFrame >= 4)
+	      iy = 1;
+    float tx = (float)ix / 4.0;
+    float ty = (float)iy / 1.0;
+    glBegin(GL_QUADS);
+
+    glTexCoord2f(tx,      ty+1.0); glVertex2i(cx+w, cy-h);
+    glTexCoord2f(tx,      ty);    glVertex2i(cx+w, cy+h);
+    glTexCoord2f(tx+.240, ty);    glVertex2i(cx-w, cy+h);
+    glTexCoord2f(tx+.240, ty+1.0); glVertex2i(cx-w, cy-h); //cy-h;
+
+    glEnd();
+    glPopMatrix();
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glDisable(GL_ALPHA_TEST);
+}
+
+//function to spawn enemies to kill
+//This is rendering(graphical) approach to colission detection
+void EnemyWaves (Global &g) {
+
+    if (health1 <=2 || health2 <=2 || health3 <=2 ||
+        health4 <=2 || health5 <=2 || health6 <=2 ||
+        health7 <=2 || health8 <=2 || health9 <=2 ||
+        health10<=2) {
+
+    if (health1 <= 2) {
+        spawnEnemy(g,0.1,2.5);
+        hit1 = detectBullEnemyColission(g);
+        if (hit1) {
+            health1++;
+        }
+    }
+
+    if (health2 <= 2) {
+        spawnEnemy(g,0.2,3.5);
+        hit2 = detectBullEnemyColission(g);
+        if (hit2) {
+            health2++;
+        }
+    }
+
+    if (health3 <= 2) {
+        spawnEnemy(g,0.3,6.5);
+        hit3 = detectBullEnemyColission(g);
+        if (hit3) {
+            health3++;
+        }
+    }
+
+    if (health4 <= 2) {
+        spawnEnemy(g,0.4,4.5);
+        hit4 = detectBullEnemyColission(g);
+        if (hit4) {
+            health4++;
+        }
+    }
+
+    if (health5 <= 2) {
+        spawnEnemy(g,0.5,9.5);
+        hit5 = detectBullEnemyColission(g);
+        if (hit5) {
+           health5++;
+        }
+    }
+
+    if (health6 <= 2) {
+        spawnEnemy(g,0.6,7.5);
+        hit6 = detectBullEnemyColission(g);
+        if (hit6) {
+            health6++;
+        }
+    }
+
+    if (health7 <= 2) {
+        spawnEnemy(g,0.7,10.5);
+        hit7 = detectBullEnemyColission(g);
+        if(hit7) {
+            health7++;
+        }
+    }
+
+    if (health8 <= 2) {
+        spawnEnemy(g,0.8,4.5);
+        hit8 = detectBullEnemyColission(g);
+        if (hit8) {
+            health8++;
+        }
+    }
+
+    if (health9 <= 2) {
+        spawnEnemy(g,0.9,6.5);
+        hit9 = detectBullEnemyColission(g);
+        if (hit9) {
+            health9++;
+        }
+    }
+
+    if (health10 <= 2) {
+        spawnEnemy(g,0.9,3.0);
+        hit10 = detectBullEnemyColission(g);
+        if (hit10) {
+            health10++;
+        }
+    }
+
+    } else {
+        //enemy health counters for each wave
+        health1 = 0;
+        health2 = 0;
+        health3 = 0;
+        health4 = 0;
+        health5 = 0;
+        health6 = 0;
+        health7 = 0;
+        health8 = 0;
+        health9 = 0;
+        health10 = 0;
+        g.exres = 800;
+        g.eyres = 600;
+    }
+
+return;
+}
